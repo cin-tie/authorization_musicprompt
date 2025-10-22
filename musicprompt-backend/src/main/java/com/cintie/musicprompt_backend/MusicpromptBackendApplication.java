7a3dca59904bfda8fb97225d90a3f2bf055e931f
cin.tie <csizegod@gmail.com>
2025-10-22 13:41:00 +0300

    Initial commit

---
+++ musicprompt-backend/src/main/java/com/cintie/musicprompt_backend/MusicpromptBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cintie.musicprompt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicpromptBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicpromptBackendApplication.class, args);
+	}
+
+}
---
+++ musicprompt-backend/src/test/java/com/cintie/musicprompt_backend/MusicpromptBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cintie.musicprompt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicpromptBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
