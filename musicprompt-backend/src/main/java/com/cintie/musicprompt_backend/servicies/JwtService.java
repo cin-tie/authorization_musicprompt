3c283179ba086c929bfd73aaa0449ff3fc2ee823
cin.tie <csizegod@gmail.com>
2025-10-24 12:25:50 +0300

    User entity, repository created, JwtService created

---
+++ musicprompt-backend/src/main/java/com/cintie/musicprompt_backend/servicies/JwtService.java
@@ -0,0 +1,45 @@
+package com.cintie.musicprompt_backend.servicies;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtService {
+
+    private static final String SECRET = "YOUR_SUPER_SECRET_KEY_CHANGE_THIS_TO_256_BIT";
+    private static final long EXPIRATION_MS = 1000 * 60 * 60;
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(key, SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token){
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parse(token);
+            return true;
+        }
+        catch (JwtException e){
+            return false;
+        }
+    }
+}
