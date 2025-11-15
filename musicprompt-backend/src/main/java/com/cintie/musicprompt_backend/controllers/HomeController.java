4ce39ee789ffb71bb04da949e88e9d70f0155c5c
cin-tie <csizegod@gmail.com>
2025-11-15 15:21:20 +0300

    Simple test front

---
+++ musicprompt-backend/src/main/java/com/cintie/musicprompt_backend/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.cintie.musicprompt_backend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+}
